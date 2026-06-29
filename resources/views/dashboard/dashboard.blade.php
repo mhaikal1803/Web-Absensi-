3aeb0d0922e167a6bfa4325404290d6f14c05b64
mhaikal1803 <muhammadhaikal180304@gmail.com>
2026-06-29 12:09:18 +0700

    add face recognition(after hosting1)

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -24,9 +24,12 @@
                 <div class="avatar">
                     @if (!empty(Auth::guard('karyawan')->user()->foto))
                         @php
-                            $path = Storage::url('uploads/karyawan/' . Auth::guard('karyawan')->user()->foto);
+                            $foto = Auth::guard('karyawan')->user()->foto;
+                            $path = \Illuminate\Support\Str::startsWith($foto, 'http')
+                                ? $foto
+                                : url(Storage::url('uploads/karyawan/' . $foto));
                         @endphp
-                        <img src="{{ url($path) }}" alt="avatar" class="imaged w64"
+                        <img src="{{ $path }}" alt="avatar" class="imaged w64"
                             style="height:64px; object-fit:cover">
                     @else
                         <img src="assets/img/sample/avatar/avatar1.jpg" alt="avatar" class="imaged w64 rounded">
@@ -154,25 +157,25 @@
                 <div class="tab-content mt-2" style="margin-bottom:100px;">
                     <div class="tab-pane fade show active" id="home" role="tabpanel">
                         <!--
-                                                                                                                <ul class="listview image-listview">
-                                                                                                                    @foreach ($historibulanini as $d)
+                                                                                                                        <ul class="listview image-listview">
+                                                                                                                            @foreach ($historibulanini as $d)
     @php
         $path = Storage::url('uploads/absensi' . $d->foto_in);
     @endphp
-                                                                                                                        <li>
-                                                                                                                            <div class="item">
-                                                                                                                                <div class="icon-box bg-primary">
-                                                                                                                                    <ion-icon name="image"></ion-icon>
-                                                                                                                                </div>
-                                                                                                                                <div class="in">
-                                                                                                                                    <div>{{ date('d-m-Y', strtotime($d->tgl_presensi)) }}</div>
-                                                                                                                                        <span class="badge badge-success">{{ $d->jam_in }}</span>
-                                                                                                                                    </div>
-                                                                                                                                </div>
-                                                                                                                        </li>
+                                                                                                                                <li>
+                                                                                                                                    <div class="item">
+                                                                                                                                        <div class="icon-box bg-primary">
+                                                                                                                                            <ion-icon name="image"></ion-icon>
+                                                                                                                                        </div>
+                                                                                                                                        <div class="in">
+                                                                                                                                            <div>{{ date('d-m-Y', strtotime($d->tgl_presensi)) }}</div>
+                                                                                                                                                <span class="badge badge-success">{{ $d->jam_in }}</span>
+                                                                                                                                            </div>
+                                                                                                                                        </div>
+                                                                                                                                </li>
     @endforeach
-                                                                                                                                                                                                                                                            </ul>
-                                                                                                                            -->
+                                                                                                                                                                                                                                                                    </ul>
+                                                                                                                                    -->
                         <style>
                             .historicontent {
                                 display: flex;
